4e773d1373c7d00e74463d1acb626dd8868358f0
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-11 16:15:09 +0500

    App.js modify

--- src/component/App.js
+++ src/component/App.js
@@ -5,7 +5,7 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-import Greeting from './greeting';
+import Greeting from './Greeting';
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<Greeting />} />),
